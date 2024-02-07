7b0fa2ffa1b1fd4abb8c0b1c026cc72370048288
TOBALBERTO <159040994+tobalberto@users.noreply.github.com>
2024-02-07 12:24:59 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let numerosecreto = 12;
+
+var chute = prompt("Digite um número de 1 e 12");
+while(chute != numerosecreto){
+    if(numerosecreto == chute){
+        alert("Você acertou!")
+    }else{
+        alert("Você é gay")
+        if(numerosecreto == chute){
+            alert("VOCE É TIMÃO E VAIII CURINTHIAAAA")
+        }else{
+            alert("VOCÊ É MANCHA GREEN VADIO!")
+        }
+    }
+    var chute = prompt("Digite um número de 1 a 12");
+}
